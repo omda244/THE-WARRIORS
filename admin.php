cbb2a704522a15e903110c97b5005d7259987fc1
joe <yousef.amr4@msa.edu.eg>
2022-04-24 17:04:33 +0200

    ayy

---
+++ admin.php
@@ -0,0 +1,120 @@
+<?php
+include_once("subjects.php");
+include_once("class.php");
+class admin extends subjects
+{
+    private $id ;
+    private $name;
+    private  $phone;
+    private $addres;
+    private $password;
+    public $subject_object = new subjects();
+    private $classobj=new classes();
+    function setsubobj( $subject_object)
+    {
+       $this->subject_object= $subject_object;
+    }
+    function getsubobj()
+    {
+        return $this->subject_object;
+    }
+    function setclass( $classobj)
+    {
+       $this->sclassobj= $classobj;
+    }
+    function getclass()
+    {
+        return $this->classobj;
+    }
+    public function setid($id)
+    {
+        $this->id=$id;
+    }
+    public function getid($id)
+    {
+        return $this->id;
+    }
+    public function set_name($name)
+    {
+        $this->name=$name;
+    }
+    public function get_name($name)
+    {
+        return $this->name;
+    }
+    public function set_phone($phone)
+    {
+        $this->phone=$phone;
+    }
+    public function get_phone($phone)
+    {
+        return $this->phone;
+    }
+    public function set_addres($addres)
+    {
+        $this->addres=$addres;
+    }
+    public function get_addres($addres)
+    {
+        return $this->addres;
+    }
+    public function set_password($password)
+    {
+        $this->password=$password;
+    }
+    public function get_password($password)
+    {
+        return $this->password;
+    }
+
+    public function login()
+    {
+
+    }
+    public function logout()
+    {
+
+    }
+    public function add_new_teacher()
+    {
+
+    }
+    public function delete_teacher()
+    {
+
+    }
+    public function add_new_student()
+    {
+
+    }
+    public function delete_student()
+    {
+
+    }
+    public function add_new_subject()
+    {
+
+    }
+    public function delete_subject()
+    {
+
+    }
+    public function add_new_class()
+    {
+
+    }
+    public function delete_class()
+    {
+
+    }
+    public function registrate()
+    {
+
+    }
+}
+
+?>
+
+
+
+
