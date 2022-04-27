a4ef7b489e10dbbf88986e283c3d2450c9c05ed9
Karim-Ghazy <100770939+Karim-Ghazy@users.noreply.github.com>
2022-04-27 04:37:28 +0200

    Add files via upload

---
+++ teacher.php
@@ -0,0 +1,12 @@
+<?php
+include_once "user1.php";
+class teacher extends user1{
+
+public function setexam(){}
+public function recordattendanceforstudents($id){}
+public function correctexam(){ }
+public function setresult(){}
+
+public $object_user1 = new user1;
+}
+?>
