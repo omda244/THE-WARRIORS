cd63b9b4c5f970eace2594338b8bab9d33407762
AliYasser1230 <aliyasserhamdy2020@gmail.com>
2022-04-27 04:06:28 +0200

    ss

--- student.php
+++ student.php
@@ -5,10 +5,10 @@ include_once("Exam.php");
 class student extends User1
 {
     public function FillAdmissionForm()
-{}
+{ }
 
     public function ViewResult($id)
-    {}
+    { }
     public function ViewSchedule($id)
     {}
     public function ViewAttendence($id)
