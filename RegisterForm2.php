ae774218a674b2ac4d3fdb6bd51b2edad5b46445
sherifmo2002 <sherif.mohammed@msa.edu.eg>
2022-06-07 03:25:19 +0200

    IF CONFIRM PASSWORD != PASSWORD

--- RegisterForm2.php
+++ RegisterForm2.php
@@ -1,7 +1,6 @@
 <?php
 include "UserFnc.php";
 $pass=Encrypt($_REQUEST["Password"],2);
-
 if (addUser($_POST["Email"],$pass,$_REQUEST["FullName"],$_REQUEST["BirthDay"],$_REQUEST["BirthdayMonth"],$_REQUEST["BirthdayYear"],$_REQUEST["role"]))
 {
 	echo "<h2>Successfully Registred</h2>";
